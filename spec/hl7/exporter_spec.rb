6ebefb263a70a5e5bcc2c2d990693334b07d6205
Enrique Carlos Mogollan <emogollan@gmail.com>
2015-05-09 13:14:24 -0700

    Using another string format to define the HL7 message in test

--- spec/hl7/exporter_spec.rb
+++ spec/hl7/exporter_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 describe HL7::Exporter::Csv do
-  let(:hl7_msh){
-    'MSH|^~\&|SOURCE|TEST_LAB_INC|HEALTH|382715520|2014100914484648||ORU^R01|0129938170710091448|P|2.3|'
+  let(:message) {
+%[MSH|^~\&|SOURCE|TEST_LAB_INC|HEALTH|382715520|2014100914484648||ORU^R01|0129938170710091448|P|2.3|
+\rPID|1||333||LastName^FirstName^MiddleInitial^SR^NickName||19760228|F||2106-3^White^HL70005^CAUC^Caucasian^L||||||
+\rOBR|1|Placer1234|Filler56789|TestID^TestName^IDSystem|||ObservationDate/Time|||||||SpecimenReceivedDate/Time||||||||ResultChangeDate/Time|||ResultStatus(From HL7 table 0123) 
+]
   }
-  let(:hl7_pid) {
-      'PID|1||333||LastName^FirstName^MiddleInitial^SR^NickName||19760228|F||2106-3^White^HL70005^CAUC^Caucasian^L||||||'
-    }
-  let(:message) { hl7_msh + "\r" + hl7_pid }
   let(:hl7_meessage) { HL7::Message.parse(message) }
   let(:exporter) { HL7::Exporter::Csv.new(hl7_meessage) }
   subject(:csv) { exporter.export.split("\n") }
